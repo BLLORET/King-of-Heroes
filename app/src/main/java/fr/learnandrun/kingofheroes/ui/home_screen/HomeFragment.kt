391d2737ab7c9279cea6855d6a51c8fa84dd4754
Nicolas Acart <nico.acart@gmail.com>
2020-09-09 19:33:58 +0200

    [Fragment] Init home fragment

---
+++ app/src/main/java/fr/learnandrun/kingofheroes/ui/home_screen/HomeFragment.kt
@@ -0,0 +1,11 @@
+package fr.learnandrun.kingofheroes.ui.home_screen
+
+import fr.learnandrun.kingofheroes.R
+import fr.learnandrun.kingofheroes.tools.android.DefaultFragment
+
+
+class HomeFragment : DefaultFragment(R.layout.fragment_home) {
+
+
+
+}
