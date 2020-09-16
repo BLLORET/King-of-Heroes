51fbcfd9c9ee25b2d2729ad0b2bdf35fe28fae13
Baptiste LLORET <baptiste.loret@hotmail.fr>
2020-09-16 22:43:20 +0200

    [Layout] Make Dice screen and refacto fight screen

---
+++ app/src/main/java/fr/learnandrun/kingofheroes/ui/dice_screen/DiceFragment.kt
@@ -0,0 +1,8 @@
+package fr.learnandrun.kingofheroes.ui.dice_screen
+
+import fr.learnandrun.kingofheroes.R
+import fr.learnandrun.kingofheroes.tools.android.DefaultFragment
+
+
+class DiceFragment : DefaultFragment(R.layout.fragment_dice) {
+}
