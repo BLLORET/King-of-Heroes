8795015bb56476269cb9b5049cac9e8e6cf50582
Nicolas Acart <nico.acart@gmail.com>
2020-09-17 16:46:58 +0200

    [StatsView] Refacto

--- app/src/main/java/fr/learnandrun/kingofheroes/ui/dice_screen/DiceFragment.kt
+++ app/src/main/java/fr/learnandrun/kingofheroes/ui/dice_screen/DiceFragment.kt
@@ -3,6 +3,5 @@ package fr.learnandrun.kingofheroes.ui.dice_screen
 import fr.learnandrun.kingofheroes.R
 import fr.learnandrun.kingofheroes.tools.android.DefaultFragment
 
-
 class DiceFragment : DefaultFragment(R.layout.fragment_dice) {
 }
