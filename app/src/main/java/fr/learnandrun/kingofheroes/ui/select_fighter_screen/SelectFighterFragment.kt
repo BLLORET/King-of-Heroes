fa3cdbbe17b70150746a516587ceed85fb68d201
Oceane Merlo <oce.merlo@gmail.com>
2020-09-18 16:25:07 +0200

    [SELECT_FIGHTER] add select_fighter fragment

---
+++ app/src/main/java/fr/learnandrun/kingofheroes/ui/select_fighter_screen/SelectFighterFragment.kt
@@ -0,0 +1,15 @@
+package fr.learnandrun.kingofheroes.ui.select_fighter_screen
+
+
+import android.os.Bundle
+import android.view.View
+import android.widget.ViewFlipper
+import fr.learnandrun.kingofheroes.R
+import fr.learnandrun.kingofheroes.tools.android.DefaultFragment
+import kotlinx.android.synthetic.main.fragment_select_fighter.*
+
+class SelectFighterFragment : DefaultFragment(R.layout.fragment_select_fighter){
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
