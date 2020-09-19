6967633fb91d4c439566cec80e2d6c0470263be6
Antonin Ginet <agintonin98@gmail.com>
2020-09-19 16:21:00 +0200

    [LAYOUT] rename layout_fight into layout_board and apply this refacto to classes

--- app/src/main/java/fr/learnandrun/kingofheroes/ui/fighter_screen/FightFragment.kt
+++
@@ -1,8 +0,0 @@
-package fr.learnandrun.kingofheroes.ui.fighter_screen
-
-import fr.learnandrun.kingofheroes.R
-import fr.learnandrun.kingofheroes.tools.android.DefaultFragment
-
-class FightFragment : DefaultFragment(R.layout.fragment_fight) {
-
-}
