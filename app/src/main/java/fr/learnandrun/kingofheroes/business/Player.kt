f017ce2de6a058635018661910ca30b64dfaa700
Antonin Ginet <agintonin98@gmail.com>
2020-09-29 18:15:18 +0200

    [PLAYER] refacto player : remove all logic from model class

--- app/src/main/java/fr/learnandrun/kingofheroes/business/Player.kt
+++ app/src/main/java/fr/learnandrun/kingofheroes/business/Player.kt
@@ -1,48 +1,21 @@
 package fr.learnandrun.kingofheroes.business
 
-import androidx.lifecycle.MutableLiveData
-import fr.learnandrun.kingofheroes.business.dice.DiceFace
-import fr.learnandrun.kingofheroes.tools.delegate.RangeDelegate
+import fr.learnandrun.kingofheroes.tools.delegate.DelegateRangeLiveData
 
 abstract class Player(
     val hero: Hero
 ) {
-    val healthLiveData: MutableLiveData<Int> = MutableLiveData(DEFAULT_HEALTH)
-    val victoryPointsLiveData: MutableLiveData<Int> = MutableLiveData(DEFAULT_POINTS)
-    val energyLiveData: MutableLiveData<Int> = MutableLiveData(DEFAULT_ENERGY)
+    val healthLiveData = DelegateRangeLiveData(DEFAULT_HEALTH, MIN_HEALTH, MAX_HEALTH)
+    val victoryPointsLiveData = DelegateRangeLiveData(DEFAULT_POINTS, MIN_POINTS, MAX_POINTS)
+    val energyLiveData = DelegateRangeLiveData(DEFAULT_ENERGY, MIN_ENERGY, MAX_ENERGY)
 
-    private var health: Int by RangeDelegate(healthLiveData, MIN_HEALTH, MAX_HEALTH)
-    private var victoryPoints: Int by RangeDelegate(victoryPointsLiveData, MIN_POINTS, MAX_POINTS)
-    private var energy: Int by RangeDelegate(energyLiveData, MIN_ENERGY, MAX_ENERGY)
+    var health: Int by healthLiveData
+    var victoryPoints: Int by victoryPointsLiveData
+    var energy: Int by energyLiveData
 
     fun isDead(): Boolean = health == MIN_HEALTH
 
     fun hasEnoughPointsToWin() = victoryPoints == MAX_POINTS
-
-    abstract suspend fun waitForRollClick(board: Board)
-    abstract suspend fun waitForReRollOrPassClick(board: Board, dices: MutableList<DiceFace?>)
-    abstract suspend fun waitForEndRollClick(board: Board)
-
-    fun increaseHealth(value: Int = 1) {
-        health += value
-    }
-    open suspend fun decreaseHealth(board: Board, value: Int = 1) {
-        health -= value
-    }
-
-    fun increaseVictoryPoints(value: Int = 1) {
-        victoryPoints += value
-    }
-    fun decreaseVictoryPoints(value: Int = 1) {
-        victoryPoints -= value
-    }
-
-    fun increaseEnergy(value: Int = 1) {
-        energy += value
-    }
-    fun decreaseEnergy(value: Int = 1) {
-        energy -= value
-    }
 
     companion object {
         const val DEFAULT_HEALTH = 10
