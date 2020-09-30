39ecc6ca5556824d3d6c72bc88a78419f8c9aa8a
Nicolas Acart <nico.acart@gmail.com>
2020-10-01 01:35:22 +0200

    [Refacto] Refacto business classes (no ui logic)

--- app/src/main/java/fr/learnandrun/kingofheroes/business/Board.kt
+++ app/src/main/java/fr/learnandrun/kingofheroes/business/Board.kt
@@ -2,16 +2,9 @@ package fr.learnandrun.kingofheroes.business
 
 import fr.learnandrun.kingofheroes.tools.delegate.DelegateNullableLiveData
 
-class Board {
+class Board(val players: List<Player>) {
 
-    lateinit var players: List<Player>
-
-    val playerInsideCityLive = DelegateNullableLiveData<Player?>(null)
+    val playerInsideCityLive = DelegateNullableLiveData<Player>(null)
     var playerInsideCity: Player? by playerInsideCityLive
-
-    fun initBoard(players: List<Player>) {
-        this.players = players
-        this.playerInsideCity = null
-    }
 
 }
