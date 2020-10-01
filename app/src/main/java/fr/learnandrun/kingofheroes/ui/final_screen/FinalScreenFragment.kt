7001ac3b0b879d73d435c51c0662cea9c10eb674
Nicolas Acart <nico.acart@gmail.com>
2020-10-01 12:56:32 +0200

    [FinalScreen] Refacto + update final screen

--- app/src/main/java/fr/learnandrun/kingofheroes/ui/final_screen/FinalScreenFragment.kt
+++ app/src/main/java/fr/learnandrun/kingofheroes/ui/final_screen/FinalScreenFragment.kt
@@ -2,22 +2,37 @@ package fr.learnandrun.kingofheroes.ui.final_screen
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.fragment.navArgs
 import fr.learnandrun.kingofheroes.R
+import fr.learnandrun.kingofheroes.business.User
 import fr.learnandrun.kingofheroes.tools.android.DefaultFragment
+import fr.learnandrun.kingofheroes.view_model.PartyViewModel
 import kotlinx.android.synthetic.main.fragment_final_screen.*
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 class FinalScreenFragment: DefaultFragment(R.layout.fragment_final_screen) {
 
-    private val args : FinalScreenFragmentArgs by navArgs()
+    private val partyViewModel: PartyViewModel by sharedViewModel()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        final_screen_title_text_view.text = if (args.isUser) getString(R.string.win_string) else getString(R.string.lose_string)
-//        final_screen_hero_image_view.setImageDrawable(args.winner.getImage(requireContext()))
-//        final_screen_replay_button.setOnClickListener {
-//            findNavController().navigate(FinalScreenFragmentDirections.actionFinalScreenFragmentToSelectFighterFragment())
-//        }
+
+        fun drawableOf(id: Int) = ContextCompat.getDrawable(requireContext(), id)
+
+        final_screen_title_text_view.text =
+            if (partyViewModel.currentPlayer is User)
+                getString(R.string.win_string)
+            else
+                getString(R.string.lose_string)
+
+        final_screen_hero_image_view.setImageDrawable(
+            drawableOf(partyViewModel.currentPlayer.hero.imageId)
+        )
+
+        final_screen_replay_button.setOnClickListener {
+            findNavController().navigate(
+                FinalScreenFragmentDirections.actionFinalScreenFragmentToSelectFighterFragment())
+        }
     }
 }
