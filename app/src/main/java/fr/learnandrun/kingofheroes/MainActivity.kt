0148adf5d4cea1c652e01f6e861a56a437dac360
Antonin Ginet <agintonin98@gmail.com>
2020-10-10 15:35:35 +0200

    [ACTIVITY] add FLAG_KEEP_SCREEN_ON

--- app/src/main/java/fr/learnandrun/kingofheroes/MainActivity.kt
+++ app/src/main/java/fr/learnandrun/kingofheroes/MainActivity.kt
@@ -1,6 +1,7 @@
 package fr.learnandrun.kingofheroes
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,6 +9,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
     }
 
 }
